dfcc28ba71c8ae597a4965bd868e76dfe92d378b
jaymin-codes <jayminmistry1008@gmail.com>
2024-05-20 19:46:57 +0530

    subscription model added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,17 +3,20 @@ import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import jwt from "jsonwebtoken";
 
 const generateAccessAndRefreshTokens = async (userId) => {
   try {
     const user = await User.findById(userId);
-
+    // console.log("1",user);
     const accessToken = user.generateAccessToken();
     const refreshToken = user.generateRefreshToken();
 
     user.refreshToken = refreshToken;
-    await user.save({ validateBeforeSave: false });
 
+    const x = await user.save({ validateBeforeSave: false });
+    // console.log("2",user.refreshToken);
+    console.log(x, "here");
     return { accessToken, refreshToken };
   } catch (error) {
     throw new ApiError(
@@ -52,7 +55,7 @@ const registerUser = asyncHandler(async (req, res) => {
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar image is required");
   }
-  
+
   let coverImageLocalPath;
   if (
     req.files &&
@@ -118,20 +121,17 @@ const loginUser = asyncHandler(async (req, res) => {
   }
 
   // password check
-  const isPasswordValid = await user.isPasswordCorrect(password)
+  const isPasswordValid = await user.isPasswordCorrect(password);
 
   if (!isPasswordValid) {
-   throw new ApiError(401, "Invalid user credentials")
-   }
-
+    throw new ApiError(401, "Invalid user credentials");
+  }
   // access and refresh token
   const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(
     user._id
   );
 
-  const loggedInUser = await User.findById(user._id).select(
-    "-password -refreshToken"
-  );
+  const loggedInUser = await User.findById(user._id).select("-password ");
 
   // send cookies
   const options = {
@@ -185,4 +185,59 @@ const logoutUser = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, {}, "User logout successfull"));
 });
 
-export { registerUser, loginUser, logoutUser };
+//
+//
+//@desc    New access token from refresh token
+//@route   POST /api/users/logout
+const newAccessToken = asyncHandler(async (req, res) => {
+  // get refresh token from cookies in frontend
+  // get refresh token from db
+
+  const incomingRefreshToken =
+    req.cookies.refreshToken || req.body.refreshToken;
+
+  if (!incomingRefreshToken) {
+    throw new ApiError(401, "Unauthorized request");
+  }
+
+  try {
+    const decodedToken = jwt.verify(
+      incomingRefreshToken,
+      process.env.REFRESH_TOKEN_SECRET
+    );
+
+    const user = await User.findById(decodedToken?._id);
+
+    if (!user) {
+      throw new ApiError(401, "Invalid refresh token");
+    }
+
+    if (incomingRefreshToken !== user?.refreshToken) {
+      throw new ApiError(401, "Refresh Token is expired or used");
+    }
+
+    const options = {
+      httpOnly: true,
+      secure: true,
+    };
+
+    const { accessToken, newRefreshToken } =
+      await generateAccessAndRefreshTokens(user._id);
+
+    return res
+      .status(200)
+      .cookie("accessToken", accessToken, options)
+      .cookie("refreshToken", newRefreshToken, options)
+      .json(
+        new ApiResponse(
+          200,
+          { accessToken, refreshToken: newRefreshToken },
+          "Access token refreshed successfully"
+        )
+      );
+  } catch (error) {
+    throw ApiError(401, error?.message || "invalid refresh token");
+  }
+});
+
+export { registerUser, loginUser, logoutUser, newAccessToken };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,12 +1,17 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js";
+import {
+  loginUser,
+  logoutUser,
+  newAccessToken,
+  registerUser,
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route("/register").post(
-  upload.fields([ 
+  upload.fields([
     {
       name: "avatar", //refers to the input field name
       maxCount: 1,
@@ -17,11 +22,12 @@ router.route("/register").post(
     },
   ]),
   registerUser
-
 );
-router.route("/login").post(loginUser)
+
+router.route("/login").post(loginUser);
 
 //secured routes
-router.route("/logout").post(verifyJWT, logoutUser)
+router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/refresh-token").post(newAccessToken);
 
 export default router;
