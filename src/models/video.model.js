3ed15c9e7dd13e58a69dbcc6f907b0f2e012b006
jaymin-codes <jayminmistry1008@gmail.com>
2024-05-22 19:18:29 +0530

    added new models

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -40,6 +40,6 @@ const videoSchema = new Schema(
   { timestamps: true }
 );
 
-videoSchema.plugin(mongooseAggregatePaginate);
+videoSchema.plugin(mongooseAggregatePaginate);//how many videos to send in a particular api call to help in pagiantion in frontend
 
 export const Video = mongoose.model("Video", videoSchema);
